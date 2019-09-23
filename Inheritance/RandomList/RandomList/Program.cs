6f79bada66dcbc514a20df1acaff29ee7b46abce
nguyenngockhanh <ngocmao99@gmail.com>
2019-09-23 10:51:09 +0700

    commit part2 lab01 doc4

---
+++ Inheritance/RandomList/RandomList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RandomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("test 1");
+            randomList.Add("test 2");
+            randomList.Add("test 3");
+            Console.WriteLine("Total Items: ");
+            foreach (string s in randomList)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine($"Item Removed: {randomList.RandomString()}");
+            Console.WriteLine("Item Remains: ");
+            foreach (string s in randomList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Inheritance/StackofStrings/StackofStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackofStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine($"Is stack empty: {stack.IsEmpty()}");
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            stack.Push("4");
+            Console.WriteLine($"Peek: {stack.Peek()}");
+            stack.Push("5");
+            Console.WriteLine($"Pop: {stack.Pop()}");
+            stack.Push("6");
+        }
+    }
+}
