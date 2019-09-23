188859da905728c238e48f0dda9304a8dbe7d8ca
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2019-09-23 10:44:48 +0700

    Working Abstract Lab

---
+++ WorkingWithAbstract/Hotel Reservation/Lab 1 Advanced Programming Doc 02 Problem 3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace Lab_1_Advanced_Programming_Doc_02_Problem_3
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {     
+            Console.WriteLine("Seasons List:\n Spring \n Summer \n Autumn \n Winter \n \n");
+            Console.WriteLine("List of Discount Type: \n VIP \n SecondVisit \n \n");
+            Console.WriteLine("Enter ''Price Per Day'', ''Number of Days'', ''Season'', " +
+                "and then ''(Discount type)'' to get the total price: \n");
+            string command = Console.ReadLine();
+            string[] commandArgs = command.Split();
+            decimal PricePerDay = Convert.ToDecimal(commandArgs[0]);
+            int NumberOfDays = Convert.ToInt32(commandArgs[1]);
+            int SeasonPrice;
+            switch (commandArgs[2])
+            {
+                case "Autumn":
+                    SeasonPrice = 1;
+                    break;
+                case "Spring":
+                    SeasonPrice = 2;
+                    break;
+                case "Winter":
+                    SeasonPrice = 3;
+                    break;
+                case "Summer":
+                    SeasonPrice = 4;
+                    break;
+                default:
+                    SeasonPrice = 1;
+                    break;
+            }
+            double DiscountPrice;
+            switch (commandArgs[3])
+            {
+                case "VIP":
+                    DiscountPrice = 0.2;
+                    break;
+                case "SecondVisit":
+                    DiscountPrice = 0.1;
+                    break;
+                default:
+                    DiscountPrice = 0;
+                    break;
+            }
+            double basisPrice = Convert.ToDouble(PricePerDay) * NumberOfDays * SeasonPrice;
+            double totalPrice = basisPrice - (basisPrice * DiscountPrice);
+            Console.WriteLine("Totol Price is: {0}", totalPrice);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkingWithAbstract/Student System/Lab 1 Advanced Programming Doc 02 Problem 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_1_Advanced_Programming_Doc_02_Problem_2
+{
+    class ValueStudent
+    {
+        public byte Age { get; set; }
+        public double Grade { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter command: Create (information), " +
+                "Show (information), or Exit to stop program");
+            Dictionary<string, ValueStudent> myDict = 
+                new Dictionary<string, ValueStudent>();
+            string command;
+            while((command = Console.ReadLine()) != "Exit")
+            {
+                string[] commandArgs = command.Split();
+                string stdName = Convert.ToString(commandArgs[1]);
+
+                switch (commandArgs[0])
+                {
+                    case "Create":
+                        if (myDict.ContainsKey(stdName))
+                            Console.WriteLine("Student information already exists");
+                        else
+                        {
+                            ValueStudent students = new ValueStudent();
+                            students.Age = Convert.ToByte(commandArgs[2]);
+                            students.Grade = Convert.ToDouble(commandArgs[3]);
+                            myDict.Add(stdName, students);
+                            Console.WriteLine("Add Student's Information successfully ...");
+                        }
+                        break;
+                    case "Show":
+                        if (!myDict.ContainsKey(stdName))
+                        {
+                            Console.WriteLine("Student not found ...");
+                        }
+                        else
+                        {
+                            foreach (var value in myDict.Values)
+                            {
+                                Console.WriteLine("{0}", value);
+                            }
+                        }
+                        break;
+                    default:
+                        Console.Write("Command Not Found... Try Again ...");
+                        break;                    
+                }
+            }
+        }
+    }
+}
